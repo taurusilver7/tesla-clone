d0f9c0a3b10de411359d0a9485eb6118a114d0bf
taurusilver7 <prasad20697@gmail.com>
2021-02-23 17:07:15 +0530

    custom btn 2 added

---
+++ src/components/ButtonSecondary/ButtonSecondary.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ButtonSecondary.css";
+
+const ButtonSecondary = ({name}) => {
+  return <button className="buttonSecondary">{name}</button>;
+};
+
+export default ButtonSecondary;
